4f4133ebdb9c3f8cc67afc00ccc62d1dc5969869
dkoelngit <dferdosi@smail.uni-koeln.de>
2025-10-28 15:48:49 +0100

    randomizer integriert

--- hausuafgaben/h1/h1_main.java
+++ hausuafgaben/h1/h1_main.java
@@ -1,3 +1,11 @@
-public class h1_main {
+package hausuafgaben.h1;
+import java.util.Random;
 
+public class h1_main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int z = rand.nextInt(50);
+        z = z + 1;
+        System.out.println(z);
+    }
 }
